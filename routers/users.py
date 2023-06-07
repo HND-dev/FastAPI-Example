54d678445e86ee31184167eb5b22aa156e376021
Hadim Ndiaye <hadim.ndiaye@epita.fr>
2023-06-07 11:26:30 +0200

    feat(userinfo): add some doc

--- routers/users.py
+++ routers/users.py
@@ -10,10 +10,28 @@ BASEURL = "https://api.github.com/users/"
 
 @router.get("/{username}")
 async def get_user_info(username: str) -> UserModel:
+    """
+    Retrieve information about a GitHub user
+
+    This endpoint send a request to the GitHub Rest API to get some information about
+    an GitHub user
+
+    This endpoint will return :
+    * User login
+    * User ID
+    * User avatar url
+    * User GitHub url
+    * User name
+        * Can be null depends on privacy settings of the user
+    * User email
+        * Can be null depends on privacy settings of the user
+    * User public repo number
+
+    """
     res = requests.get(BASEURL + username)
     if res.status_code == 200:
         return res.json()
     elif res.status_code == 404:
         raise HTTPException(404, detail="User '" + username + "' not found")
     else:
-        raise HTTPException(500, detail="Couldn't retrive info of user '" + username + "'")
+        raise HTTPException(500, detail="Couldn't retrieve info of user '" + username + "'")
